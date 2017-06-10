ff394499ddc6aae27dd399b4f768429653efdc23
John Goerzen <jgoerzen@complete.org>
2017-06-10 15:04:02 -0500

    Revert "Switch to Github for latest" for 3.8.2 branch
    This reverts commit b7d84f115b60e64a2c049206715b44ee84a55877.

--- ampache/setup/setup.sh
+++ ampache/setup/setup.sh
@@ -7,10 +7,12 @@ cd /tmp/setup
 /tmp/setup/getcomposer.sh
 mv -vi composer.phar /usr/local/bin/composer
 
+wget https://github.com/ampache/ampache/archive/3.8.2.tar.gz
+sha256sum -c < sums
+
 cd /var/www/html
-git clone https://github.com/ampache/ampache.git
-cd ampache
-git checkout master
+tar -zxvf /tmp/setup/3.8.2.tar.gz
+mv ampache-3.8.2 ampache
 
 exec rm -rf /tmp/setup
 
