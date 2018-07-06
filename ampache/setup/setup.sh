03eb9cf6bd05fbe4df9f6f3b3f1b6c53eaf25eb6
John Goerzen <jgoerzen@complete.org>
2018-07-06 14:31:17 -0500

    Setting up 3.8.8

--- ampache/setup/setup.sh
+++ ampache/setup/setup.sh
@@ -7,12 +7,12 @@ cd /tmp/setup
 /tmp/setup/getcomposer.sh
 mv -vi composer.phar /usr/local/bin/composer
 
-wget https://github.com/ampache/ampache/archive/3.8.2.tar.gz
+wget https://github.com/ampache/ampache/archive/3.8.8.tar.gz
 sha256sum -c < sums
 
 cd /var/www/html
-tar -zxvf /tmp/setup/3.8.2.tar.gz
-mv ampache-3.8.2 ampache
+tar -zxvf /tmp/setup/3.8.8.tar.gz
+mv ampache-3.8.8 ampache
 
 exec rm -rf /tmp/setup
 
